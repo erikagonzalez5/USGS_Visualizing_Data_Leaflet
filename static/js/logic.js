c53fea34563729a979eba0ab92ffec5d7458f90f
erikagonzalez5 <erikagonzalez5@gmail.com>
2022-02-19 16:12:57 -0800

    added comments

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,17 @@
 // Load the GeoJSON data.
 let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-//createMap function
-// function createMap(earthquake) {
+
    
-    //creating tile layers that will be the background of the map
+//creating tile layer that will be the background of the map
 
 const darkmap =  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+//creating map
 
 let myMap = L.map("map", {
      center: [34.0522, -118.2437],
@@ -33,7 +34,7 @@ d3.json(geoData).then(function(data) {
           weight: 0.5
         };
     }
-
+//adding colors
     function colors (magnitudes) {
         if (magnitudes > 5) {
             return "#ea822c"
@@ -49,6 +50,7 @@ d3.json(geoData).then(function(data) {
             return "#ea2c2c"
         }
     };
+    //defining markerSize
     function markerSize(magnitude) {
         if (magnitude === 0) {
           return 1;
@@ -83,7 +85,7 @@ d3.json(geoData).then(function(data) {
                 "#ea2c2c"
             ];
     
-    
+    //creating loop for legend
             for (let i = 0; i <magnitudes.length; i++) {
                 div.innerHTML += "<i style='background: " + color[i] + "'></i> "
                 + magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
